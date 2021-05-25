691a29ed6c7c50b724242c396a3003097aa1aee6
Nav Mustafee <59238786+NavonilNM@users.noreply.github.com>
2021-05-25 07:22:59 +0100

    Delete Week 8 directory

--- Week 8/mongo_use this.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-E:
-CD Program Files\MongoDB\Server\4.4\bin
-mongo --port 7000
-PAUSE
--- Week 8/mongod_use this.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-E:
-CD Program Files\MongoDB\Server\4.4\bin
-mongod --dbpath "S:\MongoDB\Data" --port 7000
-PAUSE
